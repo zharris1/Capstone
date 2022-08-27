9c55923bac5d9c4ef38234f4966706980629929a
Joseph Woodall <josephlw4@gmail.com>
2022-08-27 13:27:36 -0500

    adds module folders

--- src/run.py
+++ src/run.py
@@ -1,11 +1,3 @@
-from main.dataCollector.sourceOne import *
-from main.dataCollector.sourceTwo import *
-from main.dataCollector.sourceThree import *
-from main.dataCollector.sourceFour import *
-from main.recommender import * 
-from main.stylist import * 
-from utils.utils import *
-
 if __name__ == '__main__':
     print('Nope, the main function aint ready chief')
     
