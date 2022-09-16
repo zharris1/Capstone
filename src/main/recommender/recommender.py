8b1596aef0fd4d02cf5e0a2ce294996ef1a5ec3a
Joseph <josephlw4@gmail.com>
2022-09-15 20:35:24 -0500

    adds initial commit to Recommender

--- src/main/recommender/recommender.py
+++ src/main/recommender/recommender.py
@@ -1,3 +1,5 @@
+import torch
+
 class Recommender():
 
     """
@@ -8,6 +10,7 @@ class Recommender():
         - SVD (Singular Value Decomposition)
             - https://en.wikipedia.org/wiki/Singular_value_decomposition
             - https://web.mit.edu/be.400/www/SVD/Singular_Value_Decomposition.htm
+        - Matrix Factorization
         - Input: 
             - The Stylist's returned results, Source 4 data.
 
